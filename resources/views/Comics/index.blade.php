e3d1c3d5fb33f49bd1e89797ea01fc77e786db82
MattiaBologna <mattiabologna01@gmail.com>
2024-06-01 16:50:13 +0200

    Add: comics.index cards

--- resources/views/Comics/index.blade.php
+++ resources/views/Comics/index.blade.php
@@ -4,13 +4,34 @@
 
 @section('content')
 
-<h1>INDEX</h1>
+
+<div class="container p-5">
+    <div class="row g-4">
+        @foreach ($comics as $comic) 
+
+            <div class="card" style="width: 18rem;">
+                <img src="{{$comic->thumb}}" class="card-img-top" alt="comic thumbnail">
+                <div class="card-body">
+                    <h5 class="card-title">{{$comic->title}}</h5>
+                    <p class="card-text">{{$comic->price}}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+        @endforeach
+    </div>
+</div>
 
 @foreach ($comics as $comic) 
 
-<p> title: {{ $comic->title}}</p>
-<p> price: {{ $comic->price}}</p>
-<p> description: {{ $comic->description}}</p>
+<div class="card" style="width: 18rem;">
+  <img src="{{$comic->thumb}}" class="card-img-top" alt="comic thumbnail">
+  <div class="card-body">
+    <h5 class="card-title">{{$comic->title}}</h5>
+    <p class="card-text">{{$comic->price}}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
 
 @endforeach
 
